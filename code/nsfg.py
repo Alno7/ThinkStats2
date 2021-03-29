ecc29fe549cb3ed9f3ad54a711c6a925f06118a0
Alno7 <an394376@students.mimuw.edu.pl>
2021-03-29 19:39:19 +0200

    Update nsfg.py
    próba

--- code/nsfg.py
+++ code/nsfg.py
@@ -1,6 +1,6 @@
 """This file contains code for use with "Think Stats",
 by Allen B. Downey, available from greenteapress.com
-
+fówno
 Copyright 2010 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
